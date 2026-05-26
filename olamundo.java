55e741e1e505b2d95c14fc411401e04ae679fa63
Germany Fischer <germanyhenrique@gmail.com>
2026-05-26 20:36:08 -0300

    Fifth change on the code

--- olamundo.java
+++ olamundo.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
+
 public class olamundo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Digite o seu nome: ");
         String nome = sc.nextLine();
+        sc.close();
         System.out.println("Olá, " + nome + "!");
     }
 }
